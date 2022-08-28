7029056dc0638266f3dc06f6089d2c30dcb5d521
rbalma <balmarodrigo45@gmail.com>
2022-08-28 15:17:13 -0300

    elimina modo estricto de react

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,8 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import './styles/wieldy.less';
 import './assets/fonts/style';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
