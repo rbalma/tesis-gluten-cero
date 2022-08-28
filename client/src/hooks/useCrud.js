33ad383812e43028c0db88d9b883526aed465143
rbalma <balmarodrigo45@gmail.com>
2022-08-28 15:27:01 -0300

    agrega hook para realizar peticiones get, post, put, delete

---
+++ client/src/hooks/useCrud.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import axios from '../utils/axiosInstance';
+
+const useCrud = endpoint => {
+	const [errors, setErrors] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	const createData = async (dataForm, config = {}) => {
+		try {
+			setLoading(true);
+			const {data} = await axios.post(endpoint, dataForm, config);
+			toast.success(data?.message || 'Carga exitosa');
+			return data.data;
+		} catch (error) {
+			console.log('Error: ', error.message);
+			setErrors(error);
+      error.response?.data && toast.success(error.response.data.message );
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const readData = async (filters = {}) => {
+		try {
+			let params = { ...filters };
+			setLoading(true);
+			const { data } = await axios.get(endpoint, { params });
+			return data.data;
+		} catch (error) {
+			console.log('Error', error.message);
+			setErrors(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const updateData = async (id, dataForm, config = {}) => {
+		try {
+			setLoading(false);
+			const { data } = await axios.put(`${endpoint}/${id}`, dataForm, config);
+			toast.success(data?.message || 'Editado con Ã©xito');
+			return data.data;
+		} catch (error) {
+			console.log('Error: ', error.message);
+			setErrors(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const deleteData = async (id, dataForm = {}) => {
+		try {
+			setLoading(false);
+			const { data } = await axios.delete(`${endpoint}/${id}`, {
+				data: dataForm,
+			});
+			toast.success(data?.message || 'Eliminado con Ã©xito');
+			return data.data;
+		} catch (error) {
+			console.log('Error: ', error.message);
+			setErrors(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return [errors, loading, createData, readData, updateData, deleteData];
+};
+
+export default useCrud;
