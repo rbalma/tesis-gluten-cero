1797707787b66084184720d6de6d372f8c02a205
rbalma <balmarodrigo45@gmail.com>
2022-08-28 15:17:36 -0300

    agrega react toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
+import { ToastContainer } from 'react-toastify';
 import AppRoutes from './routes/AppRoutes';
 
-const App = () => <AppRoutes />;
+const App = () => {
+	return (
+		<>
+			<AppRoutes />
+			<ToastContainer theme='colored' pauseOnFocusLoss={false} />
+		</>
+	);
+};
 
 export default App;
