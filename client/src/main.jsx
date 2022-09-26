b91199d8d2766cbca0578c0cda9f12b665ae628d
rbalma <balmarodrigo45@gmail.com>
2022-09-26 17:56:02 -0300

    elimina estilos less que no se utilizan

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 import './assets/fonts/style';
-// import './styles/wieldy.less';
+import './styles/wieldy.less';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
