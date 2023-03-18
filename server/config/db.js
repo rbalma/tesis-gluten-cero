79607db7989debbca6fd6dbe09f219f96a8cb1fb
rbalma <balmarodrigo45@gmail.com>
2023-03-18 17:45:09 -0300

    se agrega conexión a mongo db atlas

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async() => {
     try {
-        mongoose.connect(process.env.MONGO_URL, {
+        mongoose.connect(process.env.MONGO_ATLAS, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
