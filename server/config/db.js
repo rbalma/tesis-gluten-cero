f2387ed85e1b5e6d360849b7ee0c00dc74fdc735
rbalma <balmarodrigo45@gmail.com>
2024-04-20 17:39:47 -0300

    El server apunta a la base desplegada en mongo atlas

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ import { MONGO_ATLAS, MONGO_URL } from '../config/config.js';
 const connectDB = async() => {
     try {
         mongoose.set("strictQuery", false);
-        mongoose.connect(MONGO_URL, {
+        mongoose.connect(MONGO_ATLAS, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
