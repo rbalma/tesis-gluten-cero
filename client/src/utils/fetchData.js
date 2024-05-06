0b6e63fe6b1be9ff535f4b843e35e3dd7b4e60dc
rbalma <balmarodrigo45@gmail.com>
2024-05-06 13:55:44 -0300

    Se agrega función para obtener la imagen de una categoría por la api

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -5,6 +5,9 @@ export const noticeGetImage = ( name ) => {
   return `${server}/notice-image/${name}`;
 }
 
+export const categoryGetImage = ( name ) => {
+  return `${server}/image/category/${name}`;
+}
 
 export const userGetAvatar = ( name ) => {
   return `${server}/user-avatar/${name}`;
