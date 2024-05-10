9914e1414cf0112b7b77c4a20eacb123abc189a8
rbalma <balmarodrigo45@gmail.com>
2024-05-09 21:03:13 -0300

    Se agrega componente Datepicker para utilizar con dateFns en lugar de moment

--- client/src/components/AdminDashboard/index.js
+++ client/src/components/AdminDashboard/index.js
@@ -1,6 +1,8 @@
 export * from './DataTable';
 export * from './ToolBar';
 
+export * from './DatePicker';
+
 export * from './TagVisible';
 export * from './TagNoVisible';
 export * from './getColumnSearchProps';
