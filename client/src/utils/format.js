c3f705175976be7394e5b2dfd7324da465212f97
rbalma <balmarodrigo45@gmail.com>
2024-05-09 21:10:06 -0300

    Se crean funciones para formatear las fechas

---
+++ client/src/utils/format.js
@@ -0,0 +1,12 @@
+import { format, formatDistance } from 'date-fns';
+import { es } from 'date-fns/locale';
+
+export const timeAgo = (date) => {
+	const result = formatDistance(new Date(), new Date(date), { locale: es });
+	return result ? `Hace ${result}` : '';
+};
+
+export const dateFormat = (date) => {
+	const result = format(new Date(date), 'dd MMMM, yyyy', {locale: es});
+	return result;
+}
