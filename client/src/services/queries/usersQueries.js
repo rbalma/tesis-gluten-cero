7c44d61ef482effd0b73f366e0a4c567a0420eeb
rbalma <balmarodrigo45@gmail.com>
2024-05-10 21:02:09 -0300

    Se agregan las peticiones y queries de tanstack query para los usuarios

---
+++ client/src/services/queries/usersQueries.js
@@ -0,0 +1,71 @@
+import { toast } from 'sonner';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import {
+	createUser,
+	deleteUser,
+	getUsers,
+	getUserById,
+	updateUser,
+} from '../api/usersApi';
+
+export const useGetUsers = (filters) => {
+	return useQuery({
+		queryKey: ['users', filters],
+		queryFn: () => getUsers(filters),
+	});
+};
+
+export const useGetUserById = (userId) => {
+	return useQuery({
+		queryKey: ['users', userId],
+		queryFn: () => getUserById(userId),
+		enabled: !!userId,
+		onError: (error) => console.log(error),
+	});
+};
+
+export const useCreateUserDashboard = () => {
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: createUser,
+		onSuccess: (data) => {
+			queryClient.invalidateQueries({
+				predicate: (query) =>
+					query.queryKey[0] === 'users' && query.queryKey[1]?.page >= 1,
+			});
+			toast.success(data?.message);
+		},
+		onError: () => toast.error('Error. Vuelva a intentarlo'),
+	});
+};
+
+export const useUpdateUser = (userId) => {
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: updateUser,
+		onSuccess: (data) => {
+			queryClient.setQueryData(['users', userId], data?.user);
+			queryClient.invalidateQueries({
+				predicate: (query) =>
+					query.queryKey[0] === 'users' && query.queryKey[1]?.page >= 1,
+			});
+			toast.success(data?.message);
+		},
+		onError: () => toast.error('Error. Vuelva a intentarlo'),
+	});
+};
+
+export const useDeleteUsers = () => {
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: deleteUser,
+		onSuccess: (data) => {
+			queryClient.invalidateQueries({
+				predicate: (query) =>
+					query.queryKey[0] === 'users' && query.queryKey[1]?.page >= 1,
+			});
+			toast.success(data?.message);
+		},
+		onError: () => toast.error('Error. Vuelva a intentarlo'),
+	});
+};
