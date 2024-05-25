4b44398e36ac888f44356c9bb11e001adcaffbba
rbalma <balmarodrigo45@gmail.com>
2024-05-25 14:34:54 -0300

    Se modifica el mensaje de creación de una receta

--- client/src/components/Recipe/FormRecipe/SuccessCreatedRecipe.jsx
+++ client/src/components/Recipe/FormRecipe/SuccessCreatedRecipe.jsx
@@ -1,20 +1,20 @@
-import { CheckmarkIcon } from '@/components/Checkmark/CheckmarkIcon';
-import styles from './SuccessCreatedRecipe.module.css';
 import { Link } from 'react-router-dom';
+import { CheckmarkIcon } from '@/components/Checkmark/CheckmarkIcon';
 import { IconArrowBackUp } from '@/components/Icons';
+
+import styles from './SuccessCreatedRecipe.module.css';
 
 export const SuccessCreatedRecipe = () => {
 	return (
 		<div className={styles.containerSuccessRecipe}>
 			<CheckmarkIcon />
 
-			<h1>La receta fue creada con éxito</h1>
-			{/* <h1>La receta creada está en revisión</h1> */}
-			{/* <p>
+			<h1>La receta creada está en revisión</h1>
+			<p>
 				Todas las recetas se someten a un proceso de revisión estándar.
 				<br /> Si se aprueba tu receta, aparecerá visible para los demás en
 				Gluten Cero
-			</p> */}
+			</p>
 
 			<Link to='/recetas'>
 				{' '}
