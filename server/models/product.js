3e7f67d3b43d6701134482284e59fb8c5975e2ca
rbalma <balmarodrigo45@gmail.com>
2024-07-02 17:45:16 -0300

    Se elimina el campo description

--- server/models/product.js
+++ server/models/product.js
@@ -1,30 +1,30 @@
-import mongoose from 'mongoose';
-import mongoosePaginate from 'mongoose-paginate-v2';
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
 const Schema = mongoose.Schema;
 
-const productsSchema = new Schema ({
-    marca: {
-        type: String,
-        trim: true,
-    },
-    denominacionVenta: {
-        type: String,
-        trim: true,
-    },
-    tipoProducto: {
-        type: String,
-        trim: true,
-    },
-    description : {
-        type: String,
-        trim: true,
-    },
-    estado: {
-        type: String,
-        enum: ["VIGENTE", "BAJA PROVISORIA"],
-    }
+const productsSchema = new Schema({
+  marca: {
+    type: String,
+    trim: true,
+    lowercase: true,
+  },
+  denominacionVenta: {
+    type: String,
+    trim: true,
+    lowercase: true,
+  },
+  tipoProducto: {
+    type: String,
+    trim: true,
+    lowercase: true,
+  },
+  estado: {
+    type: String,
+    trim: true,
+    lowercase: true,
+  },
 });
 
 productsSchema.plugin(mongoosePaginate);
 
-export default mongoose.model('Product', productsSchema);
+export default mongoose.model("Product", productsSchema);
