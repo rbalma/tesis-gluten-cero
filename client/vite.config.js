46b00aba9eb9e91fa5326034572dbf97a99e1d38
rbalma <balmarodrigo45@gmail.com>
2024-11-17 13:57:33 -0300

    Se agrega la configuración para convertir la aplicación en PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,65 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import vitePluginImp from 'vite-plugin-imp';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
 		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
 	},
 	plugins: [
 		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+			workbox: {
+				navigateFallbackDenylist: [/^\/api*/],
+				maximumFileSizeToCacheInBytes: 3000000,
+				runtimeCaching: [
+					{
+						urlPattern: ({ url }) => url.pathname.startsWith('/api'),
+						handler: 'NetworkOnly',
+					},
+				],
+			},
+			manifest: {
+				name: 'Gluten Cero',
+				short_name: 'Gluten Cero',
+				theme_color: '#ffffff',
+				icons: [{
+					src: '/android-chrome-192x192.png',
+					sizes:'192x192',
+					type:'image/png',
+					purpose:'favicon'
+				},
+				{
+					src:'/android-chrome-512x512.png',
+					sizes:'512x512',
+					type:'image/png',
+					purpose:'favicon'
+				},
+				{
+					src: '/apple-touch-icon.png',
+					sizes:'180x180',
+					type:'image/png',
+					purpose:'apple touch icon',
+				},
+				{
+					src: '/maskable_icon.png',
+					sizes:'512x512',
+					type:'image/png',
+					purpose:'any maskable',
+				}
+			],
+			},
+			theme_color:'#171717',
+			background_color:'#f0e7db',
+			display:"standalone",
+			scope:'/',
+			start_url:"/",
+			orientation:'portrait'
+		}),
 		vitePluginImp({
 			libList: [
 				{
